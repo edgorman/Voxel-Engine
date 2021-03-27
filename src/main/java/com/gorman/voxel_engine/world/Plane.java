05ed2d1cef040ab181068f838813afa122851560
Edward Gorman <ejgorman@gmail.com>
2021-03-27 13:23:21 +0000

    Add test for plane class

--- src/main/java/com/gorman/voxel_engine/world/Plane.java
+++ src/main/java/com/gorman/voxel_engine/world/Plane.java
@@ -14,15 +14,15 @@ public class Plane {
 
 	public Plane(Polygon p){
 		this.p = p.getCentre();
-		this.r1 = p.vertexes[1].subtract(p.vertexes[0]); //.normalise();
-		this.r2 = p.vertexes[2].subtract(p.vertexes[0]); //.normalise();
+		this.r1 = p.vertexes[1].subtract(p.vertexes[0]).normalise();
+		this.r2 = p.vertexes[2].subtract(p.vertexes[0]).normalise();
 		this.normal = this.r1.crossProduct(this.r2);
 	}
 	
 	public Plane(Vector p, Vector r1, Vector r2){
 		this.p = new Vector(p);
-		this.r1 = new Vector(r1);
-		this.r2 = new Vector(r2);
+		this.r1 = new Vector(r1).normalise();
+		this.r2 = new Vector(r2).normalise();
 		this.normal = this.r1.crossProduct(this.r2);
 	}
 }

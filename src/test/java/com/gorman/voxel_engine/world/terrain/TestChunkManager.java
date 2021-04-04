02e7c3b8d26051828f17c3b19ac965f7011ba46f
Edward Gorman <ejgorman@gmail.com>
2021-04-04 18:02:37 +0100

    Fix test cases

--- src/test/java/com/gorman/voxel_engine/world/terrain/TestChunkManager.java
+++ src/test/java/com/gorman/voxel_engine/world/terrain/TestChunkManager.java
@@ -30,14 +30,14 @@ public class TestChunkManager
         assertEquals(0, u.z, 0);
 
         Vector v = m.getChunkVector(new Vector(15, 16, 17));
-        assertEquals(0 * Chunk.size * Voxel.length, v.x, 0);
-        assertEquals(1 * Chunk.size * Voxel.length, v.y, 0);
-        assertEquals(1 * Chunk.size * Voxel.length, v.z, 0);
+        assertEquals(0 * Chunk.size, v.x, 0);
+        assertEquals(1 * Chunk.size, v.y, 0);
+        assertEquals(1 * Chunk.size, v.z, 0);
 
         Vector w = m.getChunkVector(new Vector(-2, 0, 1));
-        assertEquals(-1 * Chunk.size * Voxel.length, w.x, 0);
-        assertEquals(0 * Chunk.size * Voxel.length, w.y, 0);
-        assertEquals(0 * Chunk.size * Voxel.length, w.z, 0);
+        assertEquals(-1 * Chunk.size, w.x, 0);
+        assertEquals(0 * Chunk.size, w.y, 0);
+        assertEquals(0 * Chunk.size, w.z, 0);
     }
 
     @Test
@@ -46,9 +46,9 @@ public class TestChunkManager
         ChunkManager m = new ChunkManager(f, 1);
         
         m.getChunks(new Vector(0, 0, 0));
-        assertEquals(5 * f.maxZ, m.map.size(), 0);
+        assertEquals(9 * f.maxZ, m.map.size(), 0);
         assertNotNull(m.map.get(new Vector(0, 0, 0)));
-        assertEquals(5 * f.maxZ, m.map.size(), 0);
+        assertEquals(9 * f.maxZ, m.map.size(), 0);
     }
 
 }
